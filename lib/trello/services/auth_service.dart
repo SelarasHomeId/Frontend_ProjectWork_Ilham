f3239c6f549af38880e588cb44aaf9a0ab695fbd
Ilham Abdul Hakim <70672465+IlhamAbdul07@users.noreply.github.com>
2024-11-19 23:21:24 +0700

    initial API pattern

---
+++ lib/trello/services/auth_service.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:task_management/trello/utils/constant.dart';
+
+class AuthService {
+  // Metode untuk login
+  static Future<Map<String, dynamic>?> login(
+      String email, String password) async {
+    final url = '$apiUrl/auth/login'; // Ganti dengan endpoint login Anda
+    final uri = Uri.parse(url);
+
+    // Data body untuk permintaan POST
+    final body = jsonEncode({
+      "email": email,
+      "password": password,
+      "login_form": "mobile",
+    });
+
+    try {
+      // Permintaan POST ke endpoint
+      final response = await http.post(
+        uri,
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: body,
+      );
+
+      // Periksa status kode respons
+      if (response.statusCode == 200) {
+        // Decode JSON respons ke Map
+        final Map<String, dynamic> json = jsonDecode(response.body);
+        return json; // Kembalikan respons sebagai Map
+      } else {
+        // Jika status kode bukan 200
+        print("Error: ${response.statusCode}");
+        return null;
+      }
+    } catch (e) {
+      // Tangani kesalahan
+      print("Exception: $e");
+      return null;
+    }
+  }
+}
---
+++ lib/trello/utils/constant.dart
@@ -0,0 +1,2 @@
+const String apiUrl = "https://oryx-credible-buzzard.ngrok-free.app";
+const String appTitle = "SelarasHome ID";
