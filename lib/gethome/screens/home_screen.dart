d97dab0f6114e51dc21a084dbe626ae07d60a5ae
Ilham Abdul Hakim <70672465+IlhamAbdul07@users.noreply.github.com>
2024-11-29 23:38:32 +0700

    feat: slider banner

--- lib/gethome/screens/home_screen.dart
+++ lib/gethome/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart'; // Library alternatif
 import '/trello/screens/login_screen.dart'; // Import LoginScreen
 
 class HomeScreen extends StatelessWidget {
@@ -6,6 +7,18 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Daftar gambar untuk slider
+    final List<String> imgList = [
+      'lib/assets/slide1.png',
+      'lib/assets/slide2.png',
+      'lib/assets/slide3.png',
+      'lib/assets/slide4.png',
+      'lib/assets/slide5.png',
+    ];
+
+    final PageController _pageController =
+        PageController(); // Controller untuk PageView
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Home Screen',
@@ -28,7 +41,39 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          SizedBox(height: 20), // Jarak antara slider dan konten
+          // Slider menggunakan PageView
+          SizedBox(
+            height: 200,
+            child: PageView.builder(
+              controller: _pageController,
+              itemCount: imgList.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: EdgeInsets.symmetric(horizontal: 10),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(
+                      imgList[index],
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          SizedBox(height: 10), // Jarak antara slider dan indikator
+          // Indikator untuk PageView
+          SmoothPageIndicator(
+            controller: _pageController,
+            count: imgList.length,
+            effect: ExpandingDotsEffect(
+              dotHeight: 8,
+              dotWidth: 8,
+              activeDotColor: Colors.red,
+              dotColor: Colors.grey.shade300,
+            ),
+          ),
+          SizedBox(height: 20), // Jarak antara indikator dan konten
           Center(
             child: Text('Welcome to Home Screen!'),
           ),
